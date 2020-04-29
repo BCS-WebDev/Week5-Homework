01007886cb2aa73dfe1126d6059583c8d89c6673
Kevin Choi <kevin1choi@gmail.com>
2020-04-28 18:12:26 -0700

    Script.js additions/code-commenting & README.md update

--- Develop/script.js
+++ Develop/script.js
@@ -1,113 +1,158 @@
 
 // on dom load
 $(document).ready(function() {
+    // function to append timeblocks
     function appendTimeblocks() {
-        var container = $(".container");
-        container.addClass("mb-5");
+        var container = $(".container");  // get timeblock container
+        container.addClass("mb-5");      // add bottom margin
 
-        for (var i = 9; i < 18; i++) {
-            var timeblockRow = $("<div>");
-            timeblockRow.addClass("row");
-            timeblockRow.attr("data-rowindex", i - 9);
-            container.append(timeblockRow);
+        for (var i = 9; i < 18; i++) {      // for each hour from 9am - 17pm
+            var timeblockRow = $("<div>");      // create div
+            timeblockRow.addClass("row");      // give div row class
+            timeblockRow.attr("data-rowindex", i - 9);    // giv div dataset attribute row index for access from save button
+            container.append(timeblockRow);     // append to container
 
-            var hour = $("<div>");
-            hour.addClass("hour pt-4 text-center border-top border-dark");
-            hour.width("75px");
-            if (i <= 12) {
-                hour.text(i + " AM");
-            } else if (i > 12) {
-                hour.text((i - 12) + " PM");
+            var hour = $("<div>");      // create row for hour label
+            hour.addClass("hour pt-4 text-center border-top border-dark");  // style override
+            hour.attr("data-hourindex", i);    // for access to update time
+            hour.width("75px");      // set width
+            if (i < 12) {         // if hour less than 12
+                hour.text(i + " AM");    // set hour
+            } else if (i >= 12) {     // if hour greater than 12
+                hour.text((i - 12) + " PM");    // adjust hour to standard hour
             }
-            timeblockRow.append(hour);
+            timeblockRow.append(hour);    // append hour to current row
 
-            var event = $("<textarea>");
-            event.addClass("flex-fill border-top border-bottom border-dark")
-            if (moment().get("hour") == i) {
-                event.addClass("bg-danger");
+            var event = $("<textarea>");     // create textarea for event input
+            event.addClass("flex-fill border-top border-bottom border-dark"); // style override
+            if (moment().get("hour") == i) {      // get hour(0-24) using moment.js - compare to current timeblock
+                event.addClass("bg-danger");     // red if current hour
+                hour.text(moment().format('h:mm A'));    // display full time
             } else if (moment().get("hour") > i) {
-                event.addClass("bg-secondary");
+                event.addClass("bg-secondary");     // gray if past hours
             } else if (moment().get("hour") < i) {
-                event.addClass("bg-success");
+                event.addClass("bg-success");      // green if future hours
             }
-            timeblockRow.append(event);
+            timeblockRow.append(event);    // append event to current row
 
-            var saveButton = $("<button>");
-            saveButton.addClass("saveBtn border-dark");
-            saveButton.width("75px");
-            timeblockRow.append(saveButton);
+            var saveButton = $("<button>");    // create save button for event
+            saveButton.addClass("saveBtn border-dark");   // style override
+            saveButton.width("75px");   // set width
+            timeblockRow.append(saveButton);     // append save button to current row
 
-            var saveIcon = $("<img>");
-            saveIcon.css("z-index","1");
-            saveIcon.attr("src", "Assets/saveIcon.png");
-            saveButton.append(saveIcon);
+            var saveIcon = $("<img>");     // create img for save icon
+            saveIcon.attr("src", "Assets/saveIcon.png");   // select source
+            saveButton.append(saveIcon);   // append save icon to save button
         }
     }
 
+    // events initialized to empty strings
     var timeblockEvents = ["", "", "", "", "", "", "", "", ""];
 
+    // fill timeblock with respective events
     function fillEvents() {
-        var tempEventsArray = JSON.parse(localStorage.getItem("timeblockEvents"));
-        if (tempEventsArray == null) { return; }
+        var tempEventsArray = JSON.parse(localStorage.getItem("timeblockEvents"));  // parse stored events
+        if (tempEventsArray == null) { return; }   // return early if none exists
 
-        timeblockEvents = tempEventsArray;
-        var eventFields = $("textarea");
+        timeblockEvents = tempEventsArray;   // if exists, set to timeblockevents array
 
-        for (var i = 0; i < timeblockEvents.length; i++) {
-            eventFields[i].value = timeblockEvents[i];
+        var eventFields = $("textarea");   // get all event fields
+        for (var i = 0; i < timeblockEvents.length; i++) {   // for each event
+            eventFields[i].value = timeblockEvents[i];      // fill existing event
         }
     }
 
+    // save event tied to save button in row
     function saveEvents(rowObject) {
-        var rowIndex = rowObject.dataset.rowindex;
-        timeblockEvents[rowIndex] = rowObject.children[1].value;
+        var rowIndex = rowObject.dataset.rowindex;     // get timeblock's data index value
+        timeblockEvents[rowIndex] = rowObject.children[1].value; // get & store event from chosen timeblock to timeblockevents array
         
-        localStorage.setItem("timeblockEvents", JSON.stringify(timeblockEvents));
+        localStorage.setItem("timeblockEvents", JSON.stringify(timeblockEvents));  // store timeblockevents array in local storage
     }
 
+    // get date suffix for date in jumbotron
     function getDateSuffix() {
-        var date;
-        if (moment().date() % 10 == 1) {
-            date = "st";
-        } else if (moment().date() % 10 == 2) {
-            date = "nd";
-        } else if (moment().date() % 10 == 3) {
-            date = "rd";
+        var dateSfx;    // string to store date suffix
+        if (moment().date() % 10 == 1) {   // if date modulo 10 is 1
+            dateSfx = "st";   // suffix is st
+        } else if (moment().date() % 10 == 2) { // if date modulo 10 is 2
+            dateSfx = "nd";   // suffix is nd
+        } else if (moment().date() % 10 == 3) { // if date modulo 10 is 3
+            dateSfx = "rd";   // suffix is rd
         } else {
-            date = "th";
+            dateSfx = "th";   // suffix is th
         }
 
-        return date;
+        return dateSfx;     // return sfx
     }
 
-    function getCurrentDay(Suffix) {
+    // get current date in string form
+    function getCurrentDay(Suffix) {   // pass date suffix as argument
         var day = daysOfTheWeek[moment().day()] + ", " + monthsOfTheYear[moment().get("month")] +
-                        " " + moment().get("date") + Suffix;
+                        " " + moment().get("date") + Suffix;    // create day string
         
-        return day;
+        return day;     // return day
     }
 
-    var saveButtonBubble = $(".container");
-    saveButtonBubble.on("click", function(event) {
-        if (event.target.matches("button")) {
-            saveEvents(event.target.parentElement);
-        } else if (event.target.matches("img")) {
-            saveEvents(event.target.parentElement.parentElement);
+    // save button event bubbling
+    var saveButtonBubble = $(".container");    // get button container for onclick event
+    saveButtonBubble.on("click", function(event) {   // on click any button inside container
+        if (event.target.matches("button")) {       // if event matches button
+            saveEvents(event.target.parentElement);   // pass button's parent, row, to saveEvents
+        } else if (event.target.matches("img")) {   // if event matches img
+            saveEvents(event.target.parentElement.parentElement); // pass button's parent's parent, row, to saveEvents
         }
     });
 
+    // update current time
+    function updateTime() {
+        var updateCurrentTime = setInterval(     // set interval to update current time / reload webpage
+            function() {
+                if (moment().minute() == 0) {   // if minute equals 0 (hour has changed)
+                    var currentTimeBlock = $(".row[data-rowindex=" + (moment().hour() - 9) + "]")[0];   // get current timeblock
+                    if (currentTimeBlock) {
+                        currentTimeBlock.children[1].removeClass("bg-success");  // remove text area green
+                        currentTimeBlock.children[1].addClass("bg-danger");   // add text area red
+                    }
+
+                    var prevTimeBlock = $(".row[data-rowindex=" + (moment().hour() - 10) + "]")[0];   // get current timeblock
+                    if (prevTimeBlock) {
+                        var hour;   // hour to set
+                        if (moment().hour() < 12) {         // if hour less than 12
+                            hour = (moment().hour() - 1) + " AM";    // set hour
+                        } else if (moment().hour() == 12) {     // if houris 12
+                            hour = moment().hour() + " PM";    // set 12 PM
+                        } else if (moment().hour() > 12) {     // if hour greater than 12
+                            hour = (moment().hour() - 1 - 12) + " PM";    // adjust hour to standard hour
+                        }
+                        prevTimeBlock.children[0].textContent = hour;
+
+                        prevTimeBlock.children[1].classList.remove("bg-danger");  // remove text area red
+                        prevTimeBlock.children[1].classList.add("bg-secondary");   // add text area gray
+                    }
+                }
     
+                var currentHourElement = $(".hour[data-hourindex=" + moment().hour() + "]")[0];   // get hour of current timeblock
+                if (currentHourElement) {        // if it exists (not undefined)
+                    currentHourElement.textContent = moment().format('h:mm A');   // update time
+                }
+            },
+            60000   // every 60000 milliseconds (60 seconds)
+        );
+    }
+
+    // days & months array for current day in jumbotron
     var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var monthsOfTheYear = ["January", "February", "March", "April", "May", "June", 
                             "July", "August", "September", "October", "November", "December"];
 
-    var dateSuffix = getDateSuffix();
-    var currentDay = getCurrentDay(dateSuffix);
-    var currentDayField = $("#currentDay");
-    currentDayField.text(currentDay);
+    var dateSuffix = getDateSuffix();   // get date suffix
+    var currentDay = getCurrentDay(dateSuffix);   // get current day with date suffix
+    var currentDayField = $("#currentDay");    // get current day element
+    currentDayField.text(currentDay);      // set current day element
 
-    appendTimeblocks();
-    fillEvents();
-
+    appendTimeblocks();       // call appendTimeblocks() 
+    fillEvents();            // call fillEvents()
+    updateTime();           // call updateTime()
 
 });
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Week5-Homework
+BootCampSpot Web Development - Week 4 Homework
+
+# Notes on Quiz Schedulers
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Schedulers work like virtual personal assistants
+that keep track of and remind you of your schedule. You tell it to alert when an event
+is supposed to take place or periodically warn you of upcoming due dates. Typically,
+these schedulers take the form of a calendar because people tend to plan months ahead
+and not just days. But however big or small, the scheduler works the same way in that
+you create an event, store it, and set reminders, by the hour, day, month, or year. 
+
+# Motive & Action
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; In our case, we are tasked with creating a day
+scheduler for the typical work day. Each hour from 9AM to 5PM will have its own 
+time block where the user can store events. Each past time block will be colored 
+gray, the current time block will be colored red while also updating the current time,
+and, each future time block will be colored green. At the turn of each hour, the 
+scheduler will refresh automatically to observe these changes. 
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; The script is rather simple. And we will go 
+through the logic in no particular order.
+
+* Run script after DOM loads
+* Function to append time blocks
+    - Create & append row
+    - Create, set, & append hour to row
+    - Create, set, & append text area to row
+    - Create, set, & append save button to row
+    - Create, set, & append save icon to save button
+* Have an array for time block events
+    - Get & set events for each time block
+* Save Button
+    - Use event bubbling with the container
+    - Store time block events array
+* Set current day for jumbotron
+    - Have array for days of the week & months of the year
+    - Determine suffix for date
+    - Get day & month
+* Set interval to update current time
+    - Find current time block by hour
+    - Update current hour time text every minute
+    - When minute turns 0 (at turn of every hour)
+        - Update hour & text area color for current & previous time block
